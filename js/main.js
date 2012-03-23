a4841bc9f94cd9c7d2aaaa2cc80569fd4ed59351
Dan Dorman <dan.dorman@gmail.com>
2012-03-22 23:44:40 -0600

    Refactor link_info from object literal into function.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 $(function() {
-  var linkInfo = {
-    "http://google.com": {
-      title: "Google",
-      description: "A search engine some people occasionally use.",
-      thumbnail_url: "http://itevent.net/it-companies/wp-content/uploads/tdomf/7125/google_logo.jpg"
+  var link_info = (function() {
+    var info = {
+      "http://www.forbes.com/sites/juliabricklin/2012/03/20/why-amcs-the-walking-dead-could-be-the-perfect-tv-format/": {
+        title: "Why AMC's 'The Walking Dead' Could Be The Perfect TV Format - Forbes",
+        description: "Jon Bernthal/&#039;The Walking Dead&#039; Some fans were shocked when Shane was killed on The Walking Dead last week, but TV executives all over Hollywood were likely gleeful. Not because they don't like Jon Bernthal (the actor who played Shane), but because from a business point of view, The Walking Dead is a [...]",
+        thumbnail_url: "http://blogs-images.forbes.com/thumbnails/blog_1897/pt_1897_510_o.jpg?t=1332282846"
+      },
+
+      "http://espn.go.com/new-york/nfl/story/_/id/7718133/tim-tebow-trade-hits-snag-contract-language-source-says": {
+        title: "Jets And Tebow A Good Fit?",
+        description: "After word spread rapidly Wednesday that the New York Jets had acquired quarterback Tim Tebow from Denver Broncos for draft picks, the teams have encountered a hang-up in the language in Tebow's contract that could nullify the trade, a Broncos source tells ESPN NFL Insider Adam Schefter.",
+        thumbnail_url: "http://a.espncdn.com/media/motion/2012/0321/dm_120321_nfl_tebow_jets_fit.jpg"
+      }
+    };
+
+    return function(url_fragment) {
+      for (url in info) {
+        if (new RegExp(url_fragment.split('').join('.*')).test(url)) {
+          var url_info = info[url];
+          url_info.url = url;
+          return url_info;
+        }
+      }
     }
-  };
+  })();
 
   function add_shared_link(url) {
-    var link = linkInfo[url];
+    var link = link_info(url);
     if (link) {
       $("#shared-links").append('<li><div><h2>' + link.title + '</h2><p>' + link.description + '</p><p><img src="' + link.thumbnail_url + '"></p></div></li>');
     }
@@ -20,6 +38,8 @@ $(function() {
 
     var url = $(this).find(":text").val();
     add_shared_link(url);
+
+    $(this).find(":text").val('');
   });
 });
 
