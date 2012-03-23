f3a73d7bc5cc40c6fcc7396419388f5d26d92a58
Dan Dorman <dan.dorman@gmail.com>
2012-03-23 02:08:57 -0600

    Add PendingLinkCollection collection.

--- js/main.js
+++ js/main.js
@@ -85,6 +85,36 @@ $(function() {
     return this.$el;
   };
 
+  // PendingLinkCollection
+
+  function PendingLinkCollection() {
+    this.items = [];
+
+    this.publish = publish;
+  }
+
+  PendingLinkCollection.prototype.add = function(link) {
+    this.items.push(link);
+
+    subscribe(link, "set", (function(self) {
+      return function(property, value) {
+        if (property === "id" && value) {
+          self.remove(link);
+        }
+      };
+    })(this));
+
+    this.publish("add", link);
+  };
+
+  PendingLinkCollection.prototype.remove = function(link) {
+    var index = this.items.indexOf(link);
+    if (~index) {
+      this.items.splice(index, 1);
+      this.publish("remove", link);
+    }
+  };
+
   // main app setup and whatnot
 
   var link_info = (function() {
