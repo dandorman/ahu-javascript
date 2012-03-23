ec78a2ada9d5473d0a8df088446cd4a0199068c0
Dan Dorman <dan.dorman@gmail.com>
2012-03-23 02:23:00 -0600

    Add PendingLinkCollectionView view.

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ $(function() {
     return this.$el;
   };
 
-  // PendingLinkCollection
+  // PendingLinkCollection collection
 
   function PendingLinkCollection() {
     this.items = [];
@@ -113,6 +113,24 @@ $(function() {
       this.items.splice(index, 1);
       this.publish("remove", link);
     }
+  };
+
+  // PendingLinkCollectionView view
+
+  function PendingLinkCollectionView(collection, $el) {
+    this.collection = collection;
+    this.$el = $el;
+
+    subscribe(this.collection, "add", (function(self) {
+      return function(link) {
+        self.addPendingLink(link);
+      };
+    })(this));
+  }
+
+  PendingLinkCollectionView.prototype.addPendingLink = function(link) {
+    var pendingLinkView = new PendingLinkView(link);
+    this.$el.append(pendingLinkView.render());
   };
 
   // main app setup and whatnot
